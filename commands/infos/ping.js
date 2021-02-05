fc878c94773d8233db000c22468496741d7d3ed3
Kunal Bagaria <kb24x7@gmail.com>
2021-02-05 12:45:08 +0530

    Publish

---
+++ commands/infos/ping.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'ping',
+    aliases: [],
+    category: 'Infos',
+    utilisation: '{prefix}ping',
+
+    execute(client, message) {
+        message.channel.send(`${client.emotes.success} - Pong: **${client.ws.ping}ms** !`);
+    },
+};
---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODAyNTEwNTY1NDQ3ODI3NDk3.YAwSNA.xAQbsJlyC_vWTkVs_atBbuY4Qtc',
+        prefix: '$',
+        activity: 'Quality Music 24x7',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
