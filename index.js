8ce663029c39edd392b8a722c722611a7357cde2
Kunal Bagaria <kb24x7@gmail.com>
2021-06-02 14:26:39 +0530

    add support for stages

--- index.js
+++ index.js
@@ -1,38 +1,25 @@
-import Discord from 'discord.js'
+import { Client, Intents } from 'discord.js'
 
-const client = new Discord.Client()
-const trigger = '$'
+const client = new Client({ intents: [Intents.ALL] });
 
-client.on('ready', () => {
+client.on('ready', async () => {
     console.log("I'm ready")
 })
 
 client.on('message', async (message) => {
     if (message.author.bot) return
-    if (message.content.startsWith(trigger)) {
-        let command = message.content.split(trigger)[1].toLowerCase()
-        if (command.includes('play') && message.member.voice.channel) {
-            let toPlayUrl = message.content.split('play ')[1]
-            if (toPlayUrl) {
-                const connection = await message.member.voice.channel.join();
-                const playSong = async () => {
-                    const dispatcher = connection.play(toPlayUrl, { highWaterMark: 1024 })
-                    dispatcher.on('start', () => {
-                        console.log(toPlayUrl, 'is now playing!');
-                    })
-                    dispatcher.on('finish', () => {
-                        playSong()
-                    })
-                }
+    let messageContent = message.content
+    if (messageContent.includes('$play')){
+        let musicLink = messageContent.split('$play ')[1]
+        const channel = await client.channels.cache.get("849567857520017429");
+        const connection = await channel.join()
+        const playSong = async () => {
+            const dispatcher = await connection.play(musicLink, { highWaterMark: 2048 })
+            dispatcher.on('finish', () => {
                 playSong()
-            } else {
-                message.channel.send(`Indicate the URL of a song to play (YouTube links won't work)`)
-            }
-        } else if (command.includes('stop') && message.member.voice.channel) {
-            const connection = await message.member.voice.channel.leave();
-        } else {
-            message.channel.send('You are not in a voice channel!')
+            })
         }
+        setTimeout(playSong, 10000)
     }
 })
 
