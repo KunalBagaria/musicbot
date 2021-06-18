ec80fb54f29f08d67acd19ea0d6c1b26351fe77a
Kunal Bagaria <kb24x7@gmail.com>
2021-06-18 12:34:57 +0530

    Fix cutting out of music

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ client.on('message', async (message) => {
                 const args = message.content.split(' ').slice(1).join(" ")
                 const regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
                 if (regexp.test(args)) {
-                    const dispatcher = connection.play(ytdl(args), { bitrate: 128000 })
+                    const dispatcher = connection.play(ytdl(args, { highWaterMark: 1 << 25 }), { bitrate: 128000 })
                     dispatcher.on('start', () => getVideoInfo(args, message))
                     dispatcher.on('finish', () => playMusic())
                 } else {
@@ -63,7 +63,7 @@ client.on('message', async (message) => {
             setTimeout(() => message.channel.stopTyping(), 10000)
             const connection = await message.member.voice.channel.join()
             const url = await trending(message)
-            const dispatcher = connection.play(ytdl(url), { bitrate: 128000 })
+            const dispatcher = connection.play(ytdl(url, { highWaterMark: 1 << 25 }), { bitrate: 128000 })
         } catch (e) {
             console.log(e)
         }
